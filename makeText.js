6ac23bd05fd17dc8bedb6b538d6211e783abfb92
tjrelly <xxterrelljacksonxx@gmail.com>
2024-06-24 12:14:36 -0400

    step 2

--- makeText.js
+++ makeText.js
@@ -1,2 +1,8 @@
 /** Command-line tool to generate Markov text. */
+const markov = require("./markov")
+
+let mm = new markov.MarkovMachine("Do you love sugar cookies like I love chocolate-chip cookies")
+console.log(mm.makeText())
+
+
 
--- markov.js
+++ markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +15,43 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
-  }
+    let chains = {};
+    //key: word for this.words
+    //value: array of words that follow the key
+    //word can only appear once
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
 
+      if (chains[word]) {
+        chains[word].push(nextWord);
+      } else {
+        chains[word] = nextWord ? [nextWord] : [null];
+      }
+    }
+
+    this.chains = chains;
+  }
 
   /** return random text from chains */
+  static choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
   makeText(numWords = 100) {
-    // TODO
+    const keys = Object.keys(this.chains);
+    let key = MarkovMachine.choice(keys);
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key = MarkovMachine.choice(this.chains[key]);
+    }
+
+    return output.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
